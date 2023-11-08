4175b25069be80279a6f731c4497fa41405ad2c6
CassiaPinheiro <133119869+CassiaPinheiro@users.noreply.github.com>
2023-11-08 09:43:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let contador=0;
 
 //Estrutura de repetição - enquanto
 for(let contador= 0;contador <listaDeTeclas.leigth;contador++){
-    const tecla= listaDeTeclas[contador];
+ const tecla= listaDeTeclas[contador];
  const instrumento = tecla.classList[1];
  const idAudio= `#som_${instrumento}`;
  console.log(instrumento);
@@ -15,4 +15,13 @@ for(let contador= 0;contador <listaDeTeclas.leigth;contador++){
  tecla.onclick= function(){
 tocaSom(idAudio);
     };
+    tecla.onkeydown= function(evento){
+        console.log(evento.code);
+        if(evento.code==='enter'){
+        tecla.classList.add('ativa');
+    }
+    if(evento.code==='space'){
+        tecla.classList.add('ativa');
+    }
+    console.log(contador);
 }
